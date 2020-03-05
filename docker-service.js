49e33137a0774c7bcf65a688e6d1c1778d9935ea
Paul <paul70079@gmail.com>
2020-03-05 11:55:55 +0100

    add gotify notification support

--- docker-service.js
+++ docker-service.js
@@ -7,14 +7,20 @@ function requiereEnv(name) {
    }
    return process.env[name];
 }
-function envOrDefault(name, def) { return process.env[name] ? process.env[name] : def}
+
+function envOrDefault(name, def) { return process.env[name] ? process.env[name] : def; }
+function optionalEnv(name) { return envOrDefault(name, null); }
 
 const config = {
    "moodle_url": requiereEnv("MOODLE_URL"),
    "moodle_token": requiereEnv("MOODLE_TOKEN"),
    "whitelist": requiereEnv("MOODLE_WHITELIST").split(','),
    "path": "/data",
-   "interval": envOrDefault("SYNC_INTERVAL", 300)
+   "interval": envOrDefault("SYNC_INTERVAL", 300),
+   "gotify": {
+      "url": optionalEnv("GOTIFY_URL"),
+      "token": optionalEnv("GOTIFY_TOKEN"),
+   }
 }
 
 // wrap a request in an promise
@@ -23,22 +29,53 @@ function request(url) {
    return new Promise((resolve, reject) => {
       // select http or https module, depending on reqested url
       const lib = url.startsWith('https') ? require('https') : require('http');
-      const request = lib.get(url, (response) => {
+      const request = lib.get(url, response => {
          // handle http errors
          if (response.statusCode < 200 || response.statusCode > 299) {
-            reject(new Error('Failed to load page, status code: ' + response.statusCode));
+            reject(new Error('request: Failed to load page, status code: ' + response.statusCode));
          }
          // temporary data holder
          const body = [];
          // on every content chunk, push it to the data array
-         response.on('data', (chunk) => body.push(chunk));
+         response.on('data', chunk => body.push(chunk));
          // we are done, resolve promise with those joined chunks
-         response.on('end', () => resolve(Buffer.concat(body)));
+         response.on('end', resolve(Buffer.concat(body)));
       });
       // handle connection errors of the request
-      request.on('error', (err) => reject(err))
+      request.on('error', err => reject('request: ' + err))
    })
 }
+
+function sendNotifications(cfg, title, message) {
+   if (!cfg || !cfg.url || !cfg.token) {
+      return new Promise((resolve, reject) => {
+         reject('sendNotifications: Invalid configuration')
+      });
+   }
+   const request = require('request');
+
+   let options = {
+      method: "POST",
+      json: true,
+      body: {
+         title: title,
+         message: message,
+         priority: 5
+      }
+   }
+   let url = cfg.url + "/message?token=" + cfg.token;
+   console.log('Send notification to "' + url + '"');
+   return new Promise((resolve, reject) => {
+      request(url, options, (error, response, body) => {
+         if (error) reject('sendNotifications: ' + error + (body ? ': ' + JSON.stringify(body) : ''));
+         if (response.statusCode != 200) {
+            reject('sendNotifications: Invalid status code <' + response.statusCode + '>' + JSON.stringify(body));
+         }
+         resolve(body);
+      });
+   });
+}
+
 
 function run(config) {
    moodle.getFiles(config.moodle_url, config.moodle_token, config.whitelist)
@@ -50,24 +87,28 @@ function run(config) {
             return fs.stat(path).then(stats => {
                if (stats.mtime < dl.time) {
                   console.log("Downloading updated file " + path)
-                  return request(dl.url).then(data => fs.writeFile(path, data))
+                  return sendNotifications(config.gotify, 'Updating file in ' + dl.course, 'Updating file "' + dl.fileName + '" in module "' + dl.module + '"')
+                     .catch(error => console.log('[ERROR] ' + error))
+                     .then(request(dl.url).then(data => fs.writeFile(path, data)))
                }
-            }, (err) => {
+            }, err => {
                if (err.code === "ENOENT") {
                   return fs.mkdir(folder, { recursive: true })
                      .then(console.log("Downloading new file " + path))
+                     .then(sendNotifications(config.gotify, 'New file in ' + dl.course, 'New file "' + dl.fileName + '" in module "' + dl.module + '"'))
+                     .catch(error => console.log('[ERROR] ' + error))
                      .then(request(dl.url).then(data => fs.writeFile(path, data)));
                }
             });
          }));
-      }).then(() => console.log("Finished!"));
+      }).then(console.log("Finished!"));
 }
 
 console.log("Config: " + JSON.stringify(config));
 console.log("Starting initial run...");
 run(config);
 
-var iId = setInterval((config) => {
+var iId = setInterval(config => {
    console.log("Starting update...");
    run(config);
 }, config.interval * 1000, config);
